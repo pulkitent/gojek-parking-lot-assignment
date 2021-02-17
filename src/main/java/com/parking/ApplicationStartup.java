f7bbe240bb782af2157fb5010e618eb67232a5db
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 12:09:16 +0530

    Update startup file to have 2 ways of taking input

--- src/main/java/com/parking/ApplicationStartup.java
+++ src/main/java/com/parking/ApplicationStartup.java
@@ -1,9 +1,15 @@
 package com.parking;
 
 import com.parking.io.input.ConsoleReaderUtility;
+import com.parking.io.input.FileReaderUtility;
 
 public class ApplicationStartup {
     public static void main(String[] args) {
-        ConsoleReaderUtility.read();
+        if (args.length >= 1) {
+            String fileName = args[0];
+            FileReaderUtility.read(fileName);
+        } else {
+            ConsoleReaderUtility.read();
+        }
     }
 }
