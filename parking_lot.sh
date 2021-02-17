d74a8b3a3865fb4c78dcdeac18734a39eecbea9f
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 12:18:05 +0530

    Update run script

--- parking_lot.sh
+++ parking_lot.sh
@@ -2,6 +2,7 @@
 # Add script to run program here.
 # run, bin/parking_lot.sh can be used to launch it.
 
-./gradlew run --args="file_input.txt"
+./gradlew run
+#./gradlew run --args="file_input.txt"
 
 # Important: Above commands is just examples, please modify to suit your requirement as necessary
