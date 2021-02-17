221e8e6cda0e9ca6fe8d19a869fd6166dd82117f
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 10:24:08 +0530

    Add ApplicationStartup

---
+++ src/main/java/com/parking/ApplicationStartup.java
@@ -0,0 +1,9 @@
+package com.parking;
+
+import com.parking.io.input.ConsoleReaderUtility;
+
+public class ApplicationStartup {
+    public static void main(String[] args) {
+        ConsoleReaderUtility.read();
+    }
+}
