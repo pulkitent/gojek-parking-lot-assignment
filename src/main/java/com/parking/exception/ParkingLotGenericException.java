3117a5c456bb88269deb68b56a5f44c0cbcb6a3a
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 10:20:12 +0530

    Add custome exception class

---
+++ src/main/java/com/parking/exception/ParkingLotGenericException.java
@@ -0,0 +1,7 @@
+package com.parking.exception;
+
+public class ParkingLotGenericException extends RuntimeException {
+    public ParkingLotGenericException(String message) {
+        super(message);
+    }
+}
