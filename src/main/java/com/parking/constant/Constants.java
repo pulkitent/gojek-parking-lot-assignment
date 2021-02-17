7ec04924502581a7fe4c8d5308b27fcbef240aaa
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 10:20:40 +0530

    Add constant.java

---
+++ src/main/java/com/parking/constant/Constants.java
@@ -0,0 +1,32 @@
+package com.parking.constant;
+
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String EMPTY_STRING = "";
+    public static final String WELCOME_MESSAGE = "Welcome to parking lot";
+    public static final String EXIT = "exit";
+    public static final String PLEASE_ENTER_YOUR_COMMAND = "Please enter your command";
+    public static final String SINGLE_SPACE = " ";
+    public static final String CREATE_PARKING_LOT_COMMAND = "create_parking_lot";
+    public static final String CREATED_PARKING_LOT_MESSAGE = "Created a parking lot with ";
+    public static final String SLOTS_MESSAGE = " slots";
+    public static final String PARK = "park";
+    public static final String ALLOCATED_SLOT_NUMBER_MESSAGE = "Allocated slot number: ";
+    public static final String LEAVE_COMMAND = "leave";
+    public static final String SLOT_NUMBER_MESSAGE = "Slot number ";
+    public static final String IS_FREE_MESSAGE = " is free";
+    public static final String STATUS_COMMAND = "status";
+    public static final String STATUS_COLUMN_MESSAGE = "Slot No." + "\t" + "Registration No" + "       " + "Colour";
+    public static final String REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR_COMMAND = "registration_numbers_for_cars_with_colour";
+    public static final String COMMAND_DOESNT_EXIST_MESSAGE = "Command doesn't exist";
+    public static final String TWO_TABS = "\t\t";
+    public static final String CAR_DOESNT_EXIST = "Car with given registration number doesn't exist";
+    public static final String PARKING_SLOT_DOESNT_EXIST = "Parking slot doesn't exist";
+    public static final String PARKING_SLOT_WAS_ALREADY_EMPTY = "Parking slot was already empty";
+    public static final String SORRY_PARKING_LOT_IS_FULL = "Sorry, parking lot is full";
+    public static final String REGISTRATION_NUMBER_CAN_NOT_BE_EMPTY = "Registration number can not be empty";
+    public static final String COLOR_CAN_NOT_BE_EMPTY = "Color can not be empty";
+    public static final String SPACE_STRING = "       ";
+}
