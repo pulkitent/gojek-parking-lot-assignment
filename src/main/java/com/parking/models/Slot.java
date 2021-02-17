226231f7bee96ce70ca7dcf5a5ec769e6ada1b8c
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-17 10:21:44 +0530

    Add slot domain enity

---
+++ src/main/java/com/parking/models/Slot.java
@@ -0,0 +1,61 @@
+package com.parking.models;
+
+import java.util.Objects;
+
+import static com.parking.constant.Constants.TWO_TABS;
+import static java.util.Objects.isNull;
+
+public class Slot {
+    private final int slotNumber;
+    private Car allottedCar;
+
+    public Slot(int slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public int getSlotNumber() {
+        return slotNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Slot slot = (Slot) o;
+        return slotNumber == slot.slotNumber && allottedCar.equals(slot.allottedCar);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(slotNumber, allottedCar);
+    }
+
+    @Override
+    public String toString() {
+        return slotNumber + TWO_TABS + allottedCar;
+    }
+
+    public void assignCarToSlot(Car car) {
+        this.allottedCar = car;
+    }
+
+    boolean isEmpty() {
+        return isNull(this.allottedCar);
+    }
+
+    boolean hasCarOfGivenColor(String color) {
+        return allottedCar.hasGivenColor(color);
+    }
+
+    String getAllottedCarRegistrationNumber() {
+        return this.allottedCar.getRegistrationNumber();
+    }
+
+    boolean hasCarOfGivenNumber(String registrationNumber) {
+        return allottedCar.hasGivenRegistrationNumber(registrationNumber);
+    }
+
+    boolean hasGivenSlotId(int slotNumber) {
+        return this.slotNumber == slotNumber;
+    }
+}
